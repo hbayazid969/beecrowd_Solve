d691230ba1d5c126199ef4a2de88fd3e5c71dda7
hbayazid969 <78470516+hbayazid969@users.noreply.github.com>
2022-08-05 20:26:09 +0600

    Create 11.Simple Calculate.cpp

---
+++ BEGINNER/11.Simple Calculate.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int p1,p2;
+    double u1,u2,pr1,pr2;
+    cin>>p1>>u1>>pr1>>p2>>u2>>pr2;
+    double res = (u1*pr1)+(u2*pr2);
+    cout<<fixed<<setprecision(2)<<"VALOR A PAGAR: R$ "<<res<<endl;
+
+}
