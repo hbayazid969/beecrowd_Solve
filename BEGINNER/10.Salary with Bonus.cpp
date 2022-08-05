5c673ca99b68b91a685bec3cb80cc0397a1dc6fd
hbayazid969 <78470516+hbayazid969@users.noreply.github.com>
2022-08-05 20:25:16 +0600

    Create 10.Salary with Bonus.cpp

---
+++ BEGINNER/10.Salary with Bonus.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    double m,b;
+    cin>>s>>m>>b;
+    double res = (m+(b*15/100));
+    cout<<fixed<<setprecision(2)<<"TOTAL = R$ "<<res<<endl;
+
+}
