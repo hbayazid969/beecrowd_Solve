928eb060c260c4ca5b3fb1d7d934d813b67c2d87
hbayazid969 <78470516+hbayazid969@users.noreply.github.com>
2022-08-05 20:30:44 +0600

    Create 16.Distance Between Two Points.cpp

---
+++ BEGINNER/16.Distance Between Two Points.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double x1,x2,y1,y2;
+    cin>>x1>>y1>>x2>>y2;
+    double r1=(x2-x1);
+    double r2=(y2-y1);
+    double res = sqrt((pow(r1,2)) + (pow(r2,2)));
+    cout<<fixed<<setprecision(4)<<res<<endl;
+
+}
