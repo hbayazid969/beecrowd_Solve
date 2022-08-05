cbd0d86c07fc7fa3ff09c822ec3f0c6b4bb34fa1
hbayazid969 <78470516+hbayazid969@users.noreply.github.com>
2022-08-05 20:24:34 +0600

    Create 9.Salary.cpp

---
+++ BEGINNER/9.Salary.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,h;
+    double s;
+    cin>>n>>h>>s;
+    cout<<"NUMBER = "<<n<<endl;
+    cout<<fixed<<setprecision(2)<<"SALARY = U$ "<<h*s<<endl;
+
+}
