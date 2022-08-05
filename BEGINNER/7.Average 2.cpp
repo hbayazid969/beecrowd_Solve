5cd9ad02f6c2f9eaac16f5abfd0a3284749672e5
hbayazid969 <78470516+hbayazid969@users.noreply.github.com>
2022-08-05 20:22:47 +0600

    Create 7.Average 2.cpp

---
+++ BEGINNER/7.Average 2.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double x,y,z;
+    cin>>x>>y>>z;
+    double avg =((x*2)+ (y*3)+(z*5))/10;
+    cout<<fixed<<setprecision(1)<<"MEDIA = "<<avg<<endl;
+
+}
