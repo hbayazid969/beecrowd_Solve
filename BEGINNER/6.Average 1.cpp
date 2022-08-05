da0980f67268925c897d74d18a451dd5099d43a1
hbayazid969 <78470516+hbayazid969@users.noreply.github.com>
2022-08-05 20:21:38 +0600

    Create 6.Average 1.cpp

---
+++ BEGINNER/6.Average 1.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double x,y;
+    cin>>x>>y;
+    double avg =((x*3.5)+ (y*7.5))/(3.5+7.5);
+    cout<<fixed<<setprecision(5)<<"MEDIA = "<<avg<<endl;
+
+}
