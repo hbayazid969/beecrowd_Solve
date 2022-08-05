55e8149a87017fc1714332987fe9fe6115fda6ef
hbayazid969 <78470516+hbayazid969@users.noreply.github.com>
2022-08-05 20:27:17 +0600

    Create 12.Sphere.cpp

---
+++ BEGINNER/12.Sphere.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double pi=3.14159,r,a;
+    cin>>r;
+    a=(4.0/3*pi*pow(r,3));
+    cout<<fixed<<setprecision(3)<<"VOLUME = "<<a<<endl;
+
+}
