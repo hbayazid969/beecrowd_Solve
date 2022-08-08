a89a8be40ea2176189beb779a0f18e7bfdbdf109
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-08-08 13:02:49 +0600

    Create 44.Divisors I.cpp

---
+++ BEGINNER/44.Divisors I.cpp
@@ -0,0 +1,17 @@
+/*
+https://www.beecrowd.com.br/judge/en/problems/view/1157
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int n;
+   cin>>n;
+   for(int i=1;i<=n;i++){
+    if(n%i==0){
+        cout<<i<<endl;
+    }
+   }
+}
