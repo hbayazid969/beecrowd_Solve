a94c5b7c46308058ac373c318b4073dab158a576
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-08-08 12:55:54 +0600

    Create 40.Fixed Password.cpp

---
+++ BEGINNER/40.Fixed Password.cpp
@@ -0,0 +1,21 @@
+/*
+https://www.beecrowd.com.br/judge/en/problems/view/1114
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int x;
+   while(x!=2002){
+    cin>>x;
+    if(x!=2002){
+        cout<<"Senha Invalida"<<endl;
+    }
+    else{
+       cout<<"Acesso Permitido"<<endl;
+    }
+   }
+
+}
