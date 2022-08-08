26429bf6f891cb6fa130a0877040323e381ad432
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-08-08 12:37:25 +0600

    Create 29.Even, Odd, Positive and Negative.cpp

---
+++ BEGINNER/29.Even, Odd, Positive and Negative.cpp
@@ -0,0 +1,32 @@
+/*
+https://www.beecrowd.com.br/judge/en/problems/view/1066
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int even=0,odd=0,num,i,positive=0,negative=0;
+    for(i=0;i<5;i++){
+        cin>>num;
+        if(num%2==0){
+                even++;
+            }
+            if(num%2!=0){
+                odd++;
+            }
+            if(num>0){
+                positive++;
+            }
+            if(num<0){
+                negative++;
+            }
+
+    }
+    cout<<even<<" valor(es) par(es)"<<endl;
+    cout<<odd<<" valor(es) impar(es)"<<endl;
+    cout<<positive<<" valor(es) positivo(s)"<<endl;
+    cout<<negative<<" valor(es) negativo(s)"<<endl;
+
+    
+}
