3b45ecea77a3431ca3e66e7da86f5a7ddb8f61db
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-08-08 12:28:31 +0600

    Create 23.Multiples.cpp

---
+++ BEGINNER/23.Multiples.cpp
@@ -0,0 +1,21 @@
+/*
+https://www.beecrowd.com.br/judge/en/problems/view/1044
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int p,q;
+    cin>>p>>q;
+    if(p%q==0 || q%p==0)
+        {
+        cout<<"Sao Multiplos"<<endl;
+        }
+        else
+        {
+            cout<<"Nao sao Multiplos"<<endl;
+        }
+
+}
