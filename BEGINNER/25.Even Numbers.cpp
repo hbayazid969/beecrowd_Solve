006ae33e87a4533df033d607654ec46c50f3e26e
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-08-08 12:31:49 +0600

    Create 25.Even Numbers.cpp

---
+++ BEGINNER/25.Even Numbers.cpp
@@ -0,0 +1,16 @@
+/*
+https://www.beecrowd.com.br/judge/en/problems/view/1059
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    for(int i=2;i<=100;i++){
+        if(i%2==0){
+            cout<<i<<endl;
+        }
+    }
+
+}
