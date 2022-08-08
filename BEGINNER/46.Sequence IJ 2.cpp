da8009a7bd8288711c7cf6f022279a6d5ce920db
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-08-08 13:06:21 +0600

    Create 46.Sequence IJ 2.cpp

---
+++ BEGINNER/46.Sequence IJ 2.cpp
@@ -0,0 +1,17 @@
+/*
+https://www.beecrowd.com.br/judge/en/problems/view/1096
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,j;
+    for(i=1;i<10;i+=2){
+        for(j=7;j>=5;j--){
+           cout<<"I="<<i<<" "<<"J="<<j<<endl;
+        }
+    }
+
+}
