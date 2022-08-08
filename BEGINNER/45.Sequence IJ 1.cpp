436505a55004741d53233adecb6f5cd1050d00f0
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-08-08 13:04:18 +0600

    Create 45.Sequence IJ 1.cpp

---
+++ BEGINNER/45.Sequence IJ 1.cpp
@@ -0,0 +1,19 @@
+/*
+https://www.beecrowd.com.br/judge/en/problems/view/1095
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i=1,j=60;
+    cout<<"I="<<i<<" "<<"J="<<j<<endl;
+    while(j>0){
+        i+=3;
+        j-=5;
+        cout<<"I="<<i<<" "<<"J="<<j<<endl;
+
+    }
+
+}
